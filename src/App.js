0e32c062cc13c029f699cc900a29dc94da11a9d0
Tomy1123 <rifinyu872@ahk.jp>
2021-08-17 16:05:30 +0900

    permit audio and video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function App() {
+const getMedia = async () => {
+
+  const constraints = { audio: true, vido: true };
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+  } catch (err) {
+
+  }
+};
+
+getMedia();
+
+const App = () => {
   return <div>Hello, React!</div>;
-}
+};
 
 export default App;
